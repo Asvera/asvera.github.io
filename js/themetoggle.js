d8c830265450b4f2de751230220b1fdc611827a0
asvera <shubham35325@gmail.com>
2024-06-12 14:45:36 +0530

    add OS-prefer-theme-scheme

--- js/themetoggle.js
+++ js/themetoggle.js
@@ -1,12 +1,12 @@
 function setTheme(mode) {
     localStorage.setItem("theme-storage", mode);
     if (mode === "dark") {
-        document.getElementById("darkModeStyle").disabled=false;
-        document.getElementById("dark-mode-toggle").innerHTML ="<img src='   https://cdn-icons-png.flaticon.com/512/10480/10480655.png '>";
+        document.getElementById("darkModeStyle").disabled = false;
+        document.getElementById("dark-mode-toggle").innerHTML = "<img src='   https://cdn-icons-png.flaticon.com/512/10480/10480655.png '>";
         feather.replace()
     } else if (mode === "light") {
-        document.getElementById("darkModeStyle").disabled=true;
-        document.getElementById("dark-mode-toggle").innerHTML ="<img src='   https://cdn-icons-png.flaticon.com/512/1823/1823397.png ' >" ;
+        document.getElementById("darkModeStyle").disabled = true;
+        document.getElementById("dark-mode-toggle").innerHTML = "<img src='   https://cdn-icons-png.flaticon.com/512/1823/1823397.png ' >";
         // feather.replace()
     }
 }
@@ -19,5 +19,5 @@ function toggleTheme() {
     }
 }
 
-var savedTheme = localStorage.getItem("theme-storage") || "light";
+var savedTheme = localStorage.getItem("theme-storage") || ((window.matchMedia("(prefers-color-scheme: dark)").matches)? "dark" : "light");
 setTheme(savedTheme);
